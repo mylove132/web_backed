5e228b84b55dbde3a6ca1110b587c27e5a64ce36
刘占会 <liuzhanhui@okay.cn>
2019-05-24 18:21:10 +0800

    修改登录接口

--- apps/users/views.py
+++ apps/users/views.py
@@ -5,11 +5,11 @@ from django.db.models import QuerySet
 from django.http import JsonResponse
 from rest_framework.views import APIView
 import hashlib
-from django.utils import timezone
 from users.serializers import *
 from api.views import CustomModelView
 
 QuerySet
+
 
 def md5(name, salt='okay'):
     md5 = hashlib.md5(bytes(name, encoding='utf-8'))
@@ -17,10 +17,10 @@ def md5(name, salt='okay'):
     return md5.hexdigest()
 
 
-
 # 用于登录
 class UserLoginApiView(APIView):
     authentication_classes = []
+
     def post(self, request, format=None):
         response = {}
         data = request.data
@@ -28,22 +28,21 @@ class UserLoginApiView(APIView):
         password = data.get('password')
         print(request.data)
         if email:
-            user = User.objects.filter(email=email)
+            user = User.objects.filter(email=email).first()
             if not user:
                 response['code'] = 1004
                 response['msg'] = '用户名错误'
                 response['data'] = ''
                 return JsonResponse(response)
-            if user.first().password == password:
-                print(type(timezone.now().strftime('%Y-%m-%d %H:%M:%S')))
-                token = md5(user, salt=timezone.now().strftime('%Y-%m-%d %H:%M:%S'))
+            if user.password == password:
+                token = md5(user.email, salt=timezone.now().strftime('%Y-%m-%d %H:%M:%S'))
                 Token.objects.update_or_create(user=user,
                                                defaults={'token': token, 'update_time': timezone.now})
                 response['code'] = 0
                 response['msg'] = 'OK'
                 response['data'] = {
                     "token": token,
-                    'email':user.email
+                    'email': user.email
                 }
 
                 return JsonResponse(response)
@@ -59,6 +58,5 @@ class UserLoginApiView(APIView):
 
 
 class UserModelViewSet(CustomModelView):
-
     queryset = User.objects.order_by('-create_time')
     serializer_class = UserSerializer
